44128411232b49471862da91495b0fec5c18f05e
gulsanober <100986412+gulsanober123@users.noreply.github.com>
2023-08-17 21:06:17 +0530

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -5,7 +5,7 @@ const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Hello From gulsanober batch 3!",
+        message: "Hello From gulsanober!",
       },
       null,
       2
